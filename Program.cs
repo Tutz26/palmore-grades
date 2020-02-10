29848098cea3db01a1e494c9a43e1e1e3d67112c
Tutz26 <tutz_26@hotmail.com>
2020-02-10 09:17:57 -0700

    Se agrega el programa por primera vez

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzz_Plus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for(int i = 1; i < 100; i++){
+               bool fizz = (i % 3 == 0);
+               bool buzz = (i % 5 == 0);
+
+                    switch(fizz, buzz)
+                    {
+                        case(true, false):
+                                Console.WriteLine("Fizz");
+                            break;
+                        case(false, true):
+                                Console.WriteLine("Buzz");
+                            break;
+                        case(true, true):
+                                Console.WriteLine("FizzBuzz");
+                            break;
+                        case(false,false):
+                                Console.WriteLine(i);
+                            break;
+                    }
+           }
+       
+        }
+
+    }
+}
+
